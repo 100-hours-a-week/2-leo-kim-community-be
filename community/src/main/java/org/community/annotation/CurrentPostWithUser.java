726283819684e22c8d0548346437e45ed815591f
Leo Kim <imleokkkk@naver.com>
2025-03-25 22:23:06 +0900

    refactor: post annotation adapted, transactional refactored, README.md contents added

---
+++ community/src/main/java/org/community/annotation/CurrentPostWithUser.java
@@ -0,0 +1,12 @@
+package org.community.annotation;
+
+import java.lang.annotation.*;
+
+// lazy initialization 문제를 해결하기 위한
+// fetch join을 적용시킨 PostEntity
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface PostWithUser {
+
+}
